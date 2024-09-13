d8b5c5a52622d68da8b4ba82831b7c15cb5e6a11
MR1C10 <mauriciorodriguesoficial@gmail.com>
2024-09-12 22:16:05 -0300

    Aroldo

--- LISTA 4/L4EX01.py
+++ LISTA 4/L4EX01.py
@@ -6,10 +6,11 @@ mostre o menor.
 
 x= int(input("digite um numero:"))
 y= int(input("digite um numero:"))
+z= int(input("digite um numero:"))
 if(x==y):
     print("os numeros são iguais")
 else:
     if(x<y):
         print("o numero menor é",x)
     else:
-        print("o numero menor é",y)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+        print("o numero menor é",y)   
