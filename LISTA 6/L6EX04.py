b9f7bd6e1de895dd3a998f33dc4b3c8520c0745b
MR1C10 <mauriciorodriguesoficial@gmail.com>
2024-10-02 14:34:49 -0300

    Corrigidos

--- LISTA 6/L6EX04.py
+++ LISTA 6/L6EX04.py
@@ -8,7 +8,6 @@ for y in range(1, 11):
     z= x*y
     print(f"{x}x{y}=",z)
 
-
 #Opção para definir quantas vezes será multiplicado
 """
 x=int(input("Digite um numero"))
@@ -16,4 +15,17 @@ y=int(input("Digite um numero"))
 for y in range(1, y+1):
     z= x*y
     print(f"{x}x{y}=",z)
+"""
+
+# y começa em 1 e vai até 10.
+# A cada iteração, ele calcula a multiplicação e imprime o resultado.
+# Após imprimir, y é incrementado em 1.
+# Quando y atinge 11, o loop para.
+"""
+x=int(input("Digite um numero"))
+y=1
+while(y<=10):
+    z=x*y
+    print(f"{x}x{y}=",z)
+    y+=1
 """
--- LISTA 6/L6EX05.py
+++ LISTA 6/L6EX05.py
@@ -2,28 +2,39 @@
 5. Desenvolva um algoritmo que leia três números e mostre na tela os números
 entre os dois maiores. 
 """
-
+#Codigo feio, arrumar assim que possivel
 x=int(input("Digite um numero:"))
 y=int(input("Digite outro numero:"))
 z=int(input("Digite mais um numero:"))
 
-if x>y and x>z:
-    m1=x
-    m2=y if y>z else z
-    for a in range(m1+1, m2):
-        print(f"numeros entre {m1} e {m2} são:", a)
-elif y>x and y>z:
-    m1=y
-    m2=x if x>z else z
-    for a in range(m1+1, m2):
-        print(f"numeros entre {m1} e {m2} são:", a)
-else:
-    m1=z
-    m2=x if x>y else y
-    for a in range(m1+1, m2):
-        print(f"numeros entre {m1} e {m2} são:", a)
-if m1<m2-1:
-    for a in range(m1+1, m2):
-        print(f"numeros entre {m1} e {m2} são:", a)
+if x>y and x>z:# x e y maior 
+    if y>z:
+        for a in range(y+1,x):
+            print(f"numeros entre {y} e {x} são:", a)
+
+if x>y and x>z:# x e z maior
+    if z>y:
+        for a in range(z+1,x):
+            print(f"numeros entre {z} e {x} são:", a)
+
+if y>x and y>z:# y e x maior 
+    if x>z:
+        for a in range(x+1,y):
+            print(f"numeros entre {x} e {y} são:", a)
+
+if y>x and y>z:# y e z maior
+    if z>x:
+        for a in range(z+1,y):
+            print(f"numeros entre {z} e {y} são:", a)
+
+if z>x and z>y:# z e x maior
+    if x>y:
+        for a in range(x+1,z):
+            print(f"numeros entre {x} e {z} são:", a)
+ 
+if z>x and z>y:# z e y maior
+    if y>x:
+        for a in range(y+1,z):
+            print(f"numeros entre {y} e {z} são:", a)
 else:
     print("Não a numeros entre as variaveis")
