81b6ca148a40502772873c120630369b1918cc74
MR1C10 <mauriciorodriguesoficial@gmail.com>
2024-10-04 16:30:27 -0300

    Organizados

--- Exercicios etec/LISTA 6/L6EX04.py
+++ Exercicios etec/LISTA 6/L6EX04.py
@@ -15,10 +15,9 @@ x=int(input("Digite um numero"))
 for y in range(1, 11):
     z= x*y
     print(f"{x}x{y}=",z)
-
-#Opção para definir quantas vezes será multiplicado
 """
 
+#Opção para definir quantas vezes será multiplicado
 """
 x=int(input("Digite um numero"))
 y=int(input("Digite um numero"))
