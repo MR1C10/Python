a59a9a28842c11dc325f5ab0834def45431b3ec4
MR1C10 <mauriciorodriguesoficial@gmail.com>
2024-10-13 20:48:59 -0300

    Queria comer pizza

--- Exercicios de logica/Ex14.py
+++ Exercicios de logica/Ex14.py
@@ -2,19 +2,19 @@
 14 - Faça um algoritmo que receba um valor A e B, e troque o
 valor de A por B e o valor de B por A e imprima na tela os valores.
 """
-"""
+
 a=10
 b=20
 
-armazenamento=a # armazena o valor de a(10)
-a=b # a recebe o valor de b(20)
-b=armazenamento # b recebe o valor de a(10) 
+if a != b:
+    armazenamento=a # armazena o valor de a(10)
+    a=b # a recebe o valor de b(20)
+    b=armazenamento # b recebe o valor de a(10) 
 
 print("a=10", a)
 print("b=20", b)
+
 """
-
-
 a=10
 b=20
 
@@ -29,3 +29,4 @@ if a != b:
 
 print("a=10", a)
 print("b=20", b)
+"""
