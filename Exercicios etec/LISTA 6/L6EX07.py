8c788037c0a11b21d304a1f709c4abc2aa59f408
MR1C10 <mauriciorodriguesoficial@gmail.com>
2024-11-09 19:34:05 -0300

    ATT

--- Exercicios etec/LISTA 6/L6EX07.py
+++ Exercicios etec/LISTA 6/L6EX07.py
@@ -3,10 +3,50 @@
 que 5, mostre os números lidos. 
 """
 
+n = 0
+
+while (n < 2):
+    w = int(input("Digite um número:")) 
+    if w < 5:
+        print(w)
+        n = 1 
+        
+        y = 5
+        while (y < 11):
+            y=int(input("Digite um número:"))
+        print(y)
+        n = 2
+
+    elif w > 10:
+        print(w)
+        n = 1
+        
+        x = 10
+        while (x > 4):
+            x=int(input("Digite um número:"))
+        print(x)
+        n = 2
+
+
+"""
+x=int(input("Digite um numero:"))
+
+while not (x < 5 or x > 10):
+    x=int(input("Numero invalido! Digite um numero["))
+
+y=int(input("Digite um numero:"))
+while (x < 5 and y > 10) or (x > 10 and y < 5):
+    y=int(input("Numero invalido! Digite outro numero:"))
+
+print(f"{x}\n{y}")
+"""
+
+# Forma 1
+"""
 x=int(input("Digite um numero maior que 10:"))
 y=int(input("Digite um numero menor que 5:"))
 
-while(x<0):
+while(x<10):
     print("Numero invalido! Digite um numero maior que 10:")
     x=int(input("Digite um numero:"))
 
@@ -16,7 +56,9 @@ while(y>5):
 
 print(f"Numero maior que 10: {x}")
 print(f"Numero menor que 5: {y}")
+"""
 
+# Forma 2
 """
 x=int(input("Digite um numero maior que 10:"))
 while(x<0):
@@ -32,6 +74,8 @@ print(f"Numero maior que 10: {x}")
 print(f"Numero menor que 5:{y}")
 """
 
+
+# Forma 3
 """
 x=int(input("Digite um numero:"))
 y=int(input("Digite um numero:"))
--- Exercicios etec/LISTA 6/L6EX08.py
+++ Exercicios etec/LISTA 6/L6EX08.py
@@ -9,9 +9,23 @@ while(x>=5):
     x=int(input("digite um numero menor que 5:"))
 
 while(x<=20-2):
-    x=x+1
-    if x % 2 == 0:
+    y= x % 2
+    if y==0:
         print(x)
+        x+=2
+
+
+"""
+x=int(input("digite um numero menor que 5:"))
+while(x>=5):
+    print("Numero invalido! Digite um numero menor que 5:")
+    x=int(input("digite um numero menor que 5:"))
+
+while(x<=20-2):
+    y= x % 2
+    if y==0:
+        print(x)
+"""
 
 """
 x=int(input("digite um numero menor que 5:"))
